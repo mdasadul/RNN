f833ed2864564720b0463f222988203ede16f3d5
mdasadul <mdasadul@ualberta.ca>
2016-02-29 08:55:31 -0600

    loading the data and ploating it

--- NN_from_scratch.py
+++ NN_from_scratch.py
@@ -1,2 +1,12 @@
 import numpy as np
+import sklearn.datasets
+import sklearn
+import matplotlib.pyplot as plt
+
+#Generate a dataset and ploat it
+np.random.seed(0)
+X,y = sklearn.datasets.make_moons(200,noise=0.20)
+plt.scatter(X[:,0],X[:,1],s=40,c=y,cmap=plt.cm.Spectral)
+plt.show()
+#plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
 
